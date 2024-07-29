1699cbe80f372d6fd97139ef3f099289d5b77d7f
Hamza Masood <hamzach330@gmail.com>
2024-07-29 22:08:19 +0200

    removed some section

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -57,7 +57,7 @@ export default function Greeting() {
                             </div>
                         </div>
                     </div>
-                    <div className="greeting-image-div">
+                    {/* <div className="greeting-image-div">
                         {illustration.animated ? (
                             <DisplayLottie animationData={landingPerson} />
                         ) : (
@@ -66,7 +66,7 @@ export default function Greeting() {
                                 src={require("../../assets/images/manOnTable.svg")}
                             ></img>
                         )}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </Fade>
